b9fcb94fac4de1494f9cf5b909d2a919055d2465
giacomo accursi <giacomo.accursi@studio.unibo.it>
2020-12-22 23:10:01 +0100

    messa a posto qualche robetta

--- template/home.php
+++ template/home.php
@@ -1,8 +1,12 @@
-<?php
-    echo "Contenuto pagina"; 
-
-    //immagine con call to action "shop now"
+ <!--   //immagine con call to action "shop now"
     //h2 Best Product
     // griglia con i best product
     //immagine di saldi o roba simile
-?>
+    
+    <div class="row">
+        <div class="col-12">
+            <img src="public/img/sfondo2.jpg" class="img-fluid pb-3" alt="">
+        </div>
+    </div>
+-->
+    
