f49abf542eedfdeb6f0f6fad5c87f768dfc1d602
Andrea Acampora <andrea.acampora@studio.unibo.it>
2020-12-23 13:02:12 +0100

    commit

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,6 @@
 <?php
     session_start(); 
+    define("IMG_ROOT", "./public/img/");
     require_once("database/database.php");
     $dbh = new DatabaseHelper("localhost", "root", "", "labottega", 3306);
 ?>
