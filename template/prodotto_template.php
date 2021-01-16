dbfc1a730ce6028ebe629cf2e7a1ca19b926f156
giacomoaccursi <giacomo.accursi@gmail.com>
2021-01-16 08:36:24 +0100

    finita wishlist, manca solo da mettere a posto il design

--- template/prodotto_template.php
+++ template/prodotto_template.php
@@ -16,18 +16,37 @@
                         <?php endif; ?>
                     </p>
                     <div class="buttons">
-                        <form class="d-inline justify-content-center justify-content-md-start">
-                            <button class="btn btn-primary btn-md mt-2 mt-sm-0" type="submit">Aggiungi al carrello
+                        <form class="addToCartForm d-inline justify-content-center justify-content-md-start">
+                            <button class="btn btn-primary " type="submit">Aggiungi al carrello
                                 <i class="fas fa-shopping-cart ml-1"></i>
                             </button>
                             <input type="hidden" class="productId" value="<?php echo $prodotto["id"]; ?>">
                         </form>
-                        <form class="d-inline justify-content-center justify-content-md-start">
-                            <button class=" btn btn-primary btn-md mt-2 mt-sm-0" type="submit">Aggiungi ai desideri
-                                <i class="fas fa-heart ml-1"></i>
-                            </button>
-                            <input type="hidden" class="productId" value="<?php echo $prodotto["id"]; ?>">
-                        </form>
+
+                        <?php if (isUserLoggedIn()) : ?>
+                            <?php if (!in_array($prodotto["id"], $templateParams["wishlist"])) : ?>
+                                <form class="d-inline addToWishListForm">
+                                    <button class="btn px-3 material-tooltip-main">
+                                        <i class="far fa-heart fa-lg"></i>
+                                    </button>
+                                    <input type="hidden" class="productId" value="<?php echo $prodotto["id"]; ?>" />
+                                </form>
+                            <?php else : ?>
+                                <form class="d-inline removeFromWishListForm">
+                                    <button class="btn px-3 material-tooltip-main">
+                                        <i class="red-icon fas fa-heart fa-lg"></i>
+                                    </button>
+                                    <input type="hidden" class="productId" value="<?php echo $prodotto["id"]; ?>" />
+                                </form>
+                            <?php endif; ?>
+                        <?php else : ?>
+                            <a href="login.php">
+                                <button class="btn px-3 material-tooltip-main">
+                                    <i class="far fa-heart fa-lg"></i>
+                                </button>
+                                <input type="hidden" class="productId" value="<?php echo $prodotto["id"]; ?>" />
+                            </a>
+                        <?php endif; ?>
                     </div>
                     <p class="mt-4"><?php echo $prodotto["descrizione"]; ?></p>
                 </div>
